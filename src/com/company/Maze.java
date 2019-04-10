9c7b7645c6be471f18842799cd325ba3509e70ca
djzenma <eidma@aucegypt.edu>
2019-04-10 19:33:56 +0200

    Finished The Maze Structure

---
+++ src/com/company/Maze.java
@@ -0,0 +1,72 @@
+package com.company;
+
+public class Maze {
+    public static final int M1_CELL = 1;
+    public static final int M2_CELL = 2;
+    public static final int M3_CELL = 3;
+    private static final int SOURCE_CELL = 4;
+    private static final int TARGET_CELL = 5;
+
+    private int[][][] maze;
+    private int rows, cols;
+    private int height = 3;
+
+    public Maze(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        maze = new int[this.rows][this.cols][this.height];
+
+        for (int i=0; i<this.rows; i++) {
+            for (int j=0; j<this.cols; j++) {
+                for(int k=0; k<this.height; k++)
+                    maze[i][j][k] = 0;
+            }
+        }
+    }
+
+    public void setSource(int x, int y) {
+        for(int z = 0; z<this.height; z++)
+            maze[x][y][z] = SOURCE_CELL;
+    }
+
+    public void setTarget(int x, int y) {
+        for(int z = 0; z<this.height; z++)
+            maze[x][y][z] = TARGET_CELL;
+    }
+
+    public void setMetal(int x, int y, int metalNumber) {
+        maze[x][y][metalNumber] = 1;
+    }
+
+    public void print() {
+        String val;
+
+        for(int k=0; k<this.height; k++) {
+             for (int i=0; i<this.rows; i++) {
+                 for (int j=0; j<this.cols; j++) {
+                    switch (maze[i][j][k]) {
+                        case SOURCE_CELL:
+                            val = "S";
+                            break;
+                        case TARGET_CELL:
+                            val = "T";
+                            break;
+                        default:
+                            val = "";
+                            break;
+                    }
+                    if(val.equals(""))
+                        System.out.print(maze[i][j][k] + " ");
+                    else
+                        System.out.print(val + " ");
+                } // End of j loop
+                 System.out.println("");
+            } // End of i loop
+            int metalNum = k + 1;
+            System.out.println("\nMetal " + metalNum + "\n");
+        } // End of k loop
+    }
+
+
+
+}
